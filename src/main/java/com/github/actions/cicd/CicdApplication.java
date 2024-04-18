0848d56ddc5513690ba12a090e5bd9caa8578ae9
Dhananjay <dhananjaydshelke@gmail.com>
2024-04-18 23:52:15 +0530

    add endpoint

--- src/main/java/com/github/actions/cicd/CicdApplication.java
+++ src/main/java/com/github/actions/cicd/CicdApplication.java
@@ -2,12 +2,22 @@ package com.github.actions.cicd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/cicd")
 public class CicdApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CicdApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CicdApplication.class, args);
+    }
 
+    @GetMapping
+    public ResponseEntity<String> hello() {
+        return ResponseEntity.ok("Hello Dhananjay");
+    }
 }
