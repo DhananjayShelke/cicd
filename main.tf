e8545a8539f0bc8d72dbc7656fe9fb5d0286de31
Dhananjay <dhananjaydshelke@gmail.com>
2024-04-18 23:10:23 +0530

    have single file

---
+++ main.tf
@@ -0,0 +1,32 @@
+
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+  backend "local" {
+  }
+}
+
+provider "google" {
+  project = "cicd-420214"
+}
+
+resource "google_cloud_run_service" "first_service" {
+  location = "asia-south1"
+  name     = "ci-cd"
+  template {
+    spec {
+      containers {
+        image = "asia-south1-docker.pkg.dev/cicd-420214/cicd/artifact-registry/cicd"
+      }
+
+    }
+  }
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
